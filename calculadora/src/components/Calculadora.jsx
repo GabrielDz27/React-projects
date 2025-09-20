14ad560d50ef554fac4b8de89062f9fa4c5f6c4a
Gabriel Dziecinny Ferreira <gabidzferreira@gmail.com>
2025-09-19 23:59:01 -0300

    Adicionando arquivo de novo e container

--- calculadora/src/components/Calculadora.jsx
+++ calculadora/src/components/Calculadora.jsx
@@ -1,40 +1,44 @@
 import React from 'react'
 import './Calculadora.css'
+import { Box, Container } from '@mui/material'
 
 export default function Calculadora() {
-    return (    
-
-            <div className='wrapper'>
-                <div>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
+    return (
+        <div style={{ marginTop: "3em" }}>
+            <Box m='3' />
+            <Container maxWidth='xs'>
+                <div className='wrapper'>
+                    <div>
+                        <button className=''>AC</button>
+                        <button className=''>+/-</button>
+                        <button className=''>%</button>
+                        <button className='botaoL'>/</button>
+                    </div>
+                    <div>
+                        <button className='gray'>7</button>
+                        <button className='gray'>8</button>
+                        <button className='gray'>9</button>
+                        <button className='botaoL'>x</button>
+                    </div>
+                    <div>
+                        <button className='gray'>4</button>
+                        <button className='gray'>5</button>
+                        <button className='gray'>6</button>
+                        <button className='botaoL'>-</button>
+                    </div>
+                    <div>
+                        <button className='gray'>1</button>
+                        <button className='gray'>2</button>
+                        <button className='gray'>3</button>
+                        <button className='botaoL'>+</button>
+                    </div>
+                    <div>
+                        <button className='gray'>0</button>
+                        <button className='botaoL'>,</button>
+                        <button className=''>=</button>
+                    </div>
                 </div>
-                <div>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                </div>
-                <div>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                </div>
-                <div>
-                    <button>AC</button>
-                    <button>+/-</button>
-                    <button>%</button>
-                </div>
-                <div>
-                    <button>+</button>
-                    <button>-</button>
-                    <button>/</button>
-                </div>
-                <div>
-                    <button>x</button>
-                    <button>,</button>
-                    <button>=</button>
-                </div>
-            </div>
+            </Container>
+        </div>
     )
 }   
